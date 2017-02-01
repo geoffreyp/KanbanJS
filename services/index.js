2eb27410d0d8bef077de3fc2c3023619aba7bdb2
Geoffrey <geoffrey.pruvost@hotmail.com>
2017-02-01 18:24:02 +0100

    update the type of status when drop event

--- services/index.js
+++ services/index.js
@@ -1,10 +1,18 @@
+var models = require('../models');
+
 module.exports = function (server) {
     var io = require('socket.io')(server);
-    
+
     io.on('connection', function(client) {
         console.log('Client connected...');
 
         client.on('change_column', function(data) {
+            models.postit.findById(data.id).then(function (p) {
+                p.type = data.target;
+                p.save().then(function () {
+                    console.log("postit updated");
+                })
+            });
             console.log(data);
         });
 
