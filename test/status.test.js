3970727ea028099ff0eb883fa4462c3006d089ee
Geoffrey <geoffrey.pruvost@hotmail.com>
2017-02-16 21:45:10 +0100

    add status test

---
+++ test/status.test.js
@@ -0,0 +1,26 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../app');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('KanbanJS', function () {
+    it('test home page status', function (done) {
+        chai.request(server)
+            .get('/')
+            .end(function (err, res) {
+                res.should.have.status(200);
+                done();
+            });
+    });
+
+    it('test signup page status', function (done) {
+        chai.request(server)
+            .get('/signup')
+            .end(function (err, res) {
+                res.should.have.status(200);
+                done();
+            });
+    });
+});
